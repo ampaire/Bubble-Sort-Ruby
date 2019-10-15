7fe6b425dc4f9bd253217de4322c5864e887e4db
davitomix <davidelimg1mail.com>
2019-10-15 16:54:20 -0500

    implementing ruby sort algorythms

---
+++ bubble_sort_by.rb
@@ -0,0 +1,22 @@
+def bubble_sort_by(arr)
+  if block_given?
+    swap = false
+    while swap != true
+      swap = true
+      for i in 0..(arr.length - 2) do
+        if (yield(arr[i], arr[i+1]) > 0) 
+          arr[i], arr[i+1] = arr[i+1], arr[i]
+          swap = false
+        end
+      end
+    end
+  else
+    raise "No block given"
+  end
+  puts "------------"
+  output = arr.join(", ")
+  puts "[#{output}]"
+end
+
+#given test
+bubble_sort_by(["hey", "hello", "hi"]) {|left, right| left.length - right.length}
