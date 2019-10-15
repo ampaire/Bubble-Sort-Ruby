3a0580fa10c2ac9060081a52849504cd104c3dc6
davitomix <davidelimg1mail.com>
2019-10-15 16:24:12 -0500

    setup files

---
+++ bubble_sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(arr)
+  swap = false
+  while swap != true
+    swap = true
+    for i in 0..(arr.length-2) do
+      if(arr[i] > arr[i+1]) 
+        arr[i], arr[i+1] = arr[i+1], arr[i]      
+        swap = false
+      end
+    end
+  end
+  output = arr.join(", ")
+  return "[#{output}]"
+end
+
+puts bubble_sort([4,3,78,2,0,2])
+# result => [0,2,2,3,4,78]
